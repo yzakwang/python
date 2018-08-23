97833ea6cfcbd3fae1bea06157151e5a65b4548b
isaacwangsv <isaac_wang@jabil.com>
2018-08-22 17:29:07 -0700

    edit bmi_app for Python2

--- bmi_app.py
+++ bmi_app.py
@@ -1,13 +1,16 @@
+from __future__ import division
+
 def bmi_app():
 	height = input('How tall are you? ')
 	weight = input('How much do you weight? ')
-	bmi_val = int(weight)/(int(height)/100)**2
-	print('Your bmi is {}'.format(round(bmi_val, 2)))
-	if bmi_val < 18.5:
+	bmi_value = int(weight)/(int(height)/100)**2
+	print('Your bmi is {}'.format(bmi_value))
+	if bmi_value < 18.5:
 		print('You\'d better eat more!')
-	elif bmi_val >= 18.5 and bmi_val <= 24:
-		print('Good job!')
+	elif bmi_value >= 18.5 and bmi_value <= 24:
+		print('Good Job!')
 	else:
 		print('You\'d better take some exercises')
+
 
 bmi_app()
