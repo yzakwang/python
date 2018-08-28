4b69e0445e219980493524a8d3d20f19ef03a5cf
yzakwang <yzakwang@gmail.com>
2018-08-28 00:04:41 -0700

    edit lucky_color

--- lucky_color.py
+++ lucky_color.py
@@ -2,12 +2,16 @@ import random
 
 colors = ['red', 'blue', 'green', 'purple', 'yellow']
 luckyColor = random.choice(colors)
-
 for i in range(3):
-    print('There are {} colors'.format(colors))
-    guess = input('Guess your luck color: ')
+    print('There are {} colors.'.format(colors))
+    guess = input('What is your lucky color? ')
     if guess != luckyColor:
-        print('Seems like {} is not your luck color:('.format(guess))
+        print('Oh no! {} is not your lucky color!'.format(guess))
         colors.remove(guess)
     else:
         break
+
+if guess == luckyColor:
+    print('Great! {} is your lucky color.'.format(guess))
+else:
+    print('Actually {} is your lucky color.'.format(luckyColor))
